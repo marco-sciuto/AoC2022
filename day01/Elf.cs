2403b5952cf39d0d2ea2661c89c3837930c853da
Marco Sciuto <12072012+marco-sciuto@users.noreply.github.com>
2022-12-10 18:16:52 +0100

    Day 1

---
+++ day01/Elf.cs
@@ -0,0 +1,5 @@
+internal class Elf
+{
+    public int items {get; set;}
+    public int calories {get; set;}
+}
---
+++ day01/Program.cs
@@ -0,0 +1,28 @@
+﻿var elfs = new List<Elf>();
+var lines = File.ReadLinesAsync("input");
+
+Elf elf = CreateAdnAddElf(elfs);
+await foreach (var line in lines)
+{
+    if (string.IsNullOrWhiteSpace(line))
+    {
+        elf = CreateAdnAddElf(elfs);
+    }
+    else
+    {
+        var calories = int.Parse(line);
+        elf.calories += calories;
+        elf.items++;
+    }
+}
+Console.WriteLine($"Part 1: {elfs.Max(e => e.calories)}");
+
+var top3 = elfs.OrderByDescending(e => e.calories).Take(3).Sum(e => e.calories);
+Console.WriteLine($"Part 2: {top3}");
+
+static Elf CreateAdnAddElf(List<Elf> elfs)
+{
+    var elf = new Elf();
+    elfs.Add(elf);
+    return elf;
+}
