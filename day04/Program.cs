c597c309bc25273931c706c6206fe3bc33a3d3f3
Marco Sciuto <12072012+marco-sciuto@users.noreply.github.com>
2022-12-11 13:10:45 +0100

    Day 4

---
+++ day04/Program.cs
@@ -0,0 +1,44 @@
+﻿var lines = File.ReadLinesAsync("input");
+var part1 = 0;
+var part2 = 0;
+await foreach (var line in lines)
+{
+    var areas = line.Split(',');
+    var zone1 = areas[0].Split('-').Select(c => int.Parse(c));
+    var zone2 = areas[1].Split('-').Select(c => int.Parse(c));
+    if (IsIncluded(zone1, zone2) || IsIncluded(zone2, zone1))
+    {
+        part1++;
+    }
+    if (IsOverlapping(zone1, zone2) || IsIncluded(zone2, zone1))
+    {
+        part2++;
+    }
+}
+
+Console.WriteLine($"Part 1: {part1}");
+Console.WriteLine($"Part 2: {part2}");
+
+bool IsIncluded(IEnumerable<int> zoneA, IEnumerable<int> zoneB)
+{
+    if (zoneA.First() >= zoneB.First() && zoneA.Last() <= zoneB.Last())
+    {
+        return true;
+    }
+    return false;
+}
+
+bool IsOverlapping(IEnumerable<int> zoneA, IEnumerable<int> zoneB)
+{
+    return IsContained(zoneA.First(), zoneB) || IsContained(zoneA.First(), zoneB) ||
+        IsContained(zoneB.First(), zoneA) || IsContained(zoneB.First(), zoneA);
+}
+
+bool IsContained(int point, IEnumerable<int> zone)
+{
+    if(point >= zone.First() && point <= zone.Last())
+    {
+        return true;
+    }
+    return false;
+}
